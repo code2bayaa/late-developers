55c00f3da9b3ec305d6af424cee4840934467a6f
code2bayaa <latedevelopers@hotmail.com>
2025-01-30 21:45:31 +0300

    account 02

--- src/app/store/search/page.jsx
+++ src/app/store/search/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import { useState, useEffect } from "react"
 import { useSearchParams } from 'next/navigation';
-import PRODUCTS from "../../components/products"
+import PRODUCTS from "@/components/products"
 
 const SEARCH = () => {
 
