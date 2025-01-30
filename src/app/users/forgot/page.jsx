f3a71b4ec59f9f9a1617cf7b9c5e34af8e97b3c0
code2bayaa <latedevelopers@hotmail.com>
2025-01-30 22:09:56 +0300

    account 05

--- src/app/users/forgot/page.jsx
+++ src/app/users/forgot/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {useRouter} from "next/navigation"
 import {login, sign_up, forgot_password} from "@/assets"
 import swal from "sweetalert"
